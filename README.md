22cc91bca728326e9ac47600d3735d8a5fdf9d0a
Paul Shields <pkshields@gmail.com>
2019-04-22 11:32:32 +0100

    Initial commit, moving the slightly modified starter Angular project to a new repository to separate it from the GitHub Pages repo.

---
+++ README.md
@@ -0,0 +1,27 @@
+# PaulShields.dev
+
+This is simply the source and GitHub Pages repo for my portfolio website, [PaulShields.dev](https://paulshields.dev).
+
+## Development
+
+#### Run local server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+### Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+### Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+### Lint the source files
+
+Run a static analysis check over the source files by executing `ng lint`.
+
+### Deploy
+
+Deployment should be done by the CI server. However, if it doe sneed to be executed locally...
+
+`[TODO]`
---
+++ src/karma.conf.js
@@ -0,0 +1,37 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+ 
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-firefox-launcher'),
+      require('karma-junit-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    junitReporter: {
+      outputDir: '../test-reports/junit',
+      outputFile: 'test-report.xml',
+      useBrowserName: false,
+    },
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: [ '-headless' ],
+      },
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['FirefoxHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
+  });
+};
