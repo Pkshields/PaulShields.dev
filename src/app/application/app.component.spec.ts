6430d7ab21bd60b0e3f4970bb960c28afe2a1d8a
Paul Shields <pkshields@gmail.com>
2019-04-23 21:30:42 +0100

    Slight refactoring to better organise CSS files, make tests easier to read and write.

--- src/app/application/app.component.spec.ts
+++ src/app/application/app.component.spec.ts
@@ -1,19 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { Type } from '@angular/core';
 
 describe('AppComponent', () => {
-  it('should create the app', () => {
-    const app = componentFor(AppComponent);
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title in a h1 and h2 tag', () => {
-    const dom = domFor(AppComponent);
-    expect(dom.querySelector('h1').textContent).toContain('Paul Shields');
-    expect(dom.querySelector('h2').textContent).toContain('Developer');
-  });
+  let component: AppComponent;
+  let dom: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,16 +15,20 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    component = fixture.debugElement.componentInstance;
+    dom = fixture.debugElement.nativeElement;
   }));
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render title in a h1 and h2 tag', () => {
+    expect(dom.querySelector('h1').textContent).toContain('Paul Shields');
+    expect(dom.querySelector('h2').textContent).toContain('Developer');
+  });
 });
-
-function componentFor<T>(component: Type<T>): T {
-  const fixture = TestBed.createComponent(AppComponent);
-  return fixture.debugElement.componentInstance;
-}
-
-function domFor<T>(component: Type<T>): HTMLElement {
-  const fixture = TestBed.createComponent(AppComponent);
-  fixture.detectChanges();
-  return fixture.debugElement.nativeElement;
-}
