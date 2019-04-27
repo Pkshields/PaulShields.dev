e6f3696fea3820cdc4055fbbbeaf4773e97cca02
Paul Shields <pkshields@gmail.com>
2019-04-27 17:35:45 +0100

    Added the initial content to the Skills page via a cards based design.

--- src/app/skills/skills.component.spec.ts
+++ src/app/skills/skills.component.spec.ts
@@ -27,4 +27,31 @@ describe('SkillsComponent', () => {
   it('should create the skills component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain 4 sections of skills', () => {
+    const elements = dom.querySelectorAll('.card');
+    expect(elements.length).toEqual(4);
+  });
+
+  it('should contain a list of skills in each section', () => {
+    const elements = dom.querySelectorAll('.card');
+
+    elements.forEach((value, key, parent) => {
+      expect(value.querySelector('li')).not.toBeNull();
+    });
+  });
+
+  it('should contain a different header in each section of skills', () => {
+    const headerIconClasses: string[] = [];
+    const skillCards = dom.querySelectorAll('.card');
+
+    skillCards.forEach((card, key, parent) => {
+      const skillCardHeader = card.querySelector('.card-title > i');
+      const iconExistsOnAnotherCard = headerIconClasses.find((value, index, obj) => value === skillCardHeader.className);
+
+      expect(iconExistsOnAnotherCard).not.toBeTruthy();
+
+      headerIconClasses.push(skillCardHeader.className);
+    });
+  });
 });
