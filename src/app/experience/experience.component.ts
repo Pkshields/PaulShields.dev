d00e842973311a86a06d1db6d18fae16d825031b
Paul Shields <pkshields@gmail.com>
2019-04-28 15:54:33 +0100

    Added mobile support to the Experience page.

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.scss']
+  styleUrls: ['./experience.component.scss', './experience.component.mobile.scss']
 })
 export class ExperienceComponent implements OnInit {
 
