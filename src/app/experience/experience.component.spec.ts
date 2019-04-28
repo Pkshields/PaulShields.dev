246f063a74c93e080030b8486a952b88f4aea946
Paul Shields <pkshields@gmail.com>
2019-04-28 15:28:16 +0100

    Added tests to the Experience page.

--- src/app/experience/experience.component.spec.ts
+++ src/app/experience/experience.component.spec.ts
@@ -27,4 +27,47 @@ describe('ExperienceComponent', () => {
   it('should create the skills component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain a timeline showing my experience', () => {
+    const elements = dom.querySelectorAll('ul');
+    expect(elements.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should contain a timeline element for Instil Software', () => {
+    runElementWithHeaderExistsTest('Instil Software');
+  });
+
+  it('should contain a timeline element for Bitnet', () => {
+    runElementWithHeaderExistsTest('Bitnet');
+  });
+
+  it('should contain a timeline element for CME Group', () => {
+    runElementWithHeaderExistsTest('CME Group');
+  });
+
+  it('should contain a timeline element for Level Seven', () => {
+    runElementWithHeaderExistsTest('Level Seven');
+  });
+
+  it('should contain a link to my full cv', () => {
+    const elements = dom.querySelectorAll('a');
+
+    let result = false;
+    elements.forEach((value, key, parent) => {
+      result = result || value.href.search('assets/cv/CV-PaulShields.pdf') !== -1;
+    });
+
+    expect(result).toBeTruthy();
+  });
+
+  function runElementWithHeaderExistsTest(header: string) {
+    const elements = dom.querySelectorAll('li > .content > h4');
+
+    let result = false;
+    elements.forEach((value, key, parent) => {
+      result = result || value.textContent === header;
+    });
+
+    expect(result).toBeTruthy();
+  }
 });
