1d8ab3239c81ac8bb71f21799e224f3d980eac2f
Paul Shields <pkshields@gmail.com>
2019-05-01 21:05:21 +0100

    Added a 404 redirect to redirect any broken urls to home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
   { path: 'skills',  component: SkillsComponent },
   { path: 'projects',  component: ProjectsComponent },
   { path: 'contact',  component: ContactComponent },
+  { path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
