2ec9b6e0bafb12776ab42a3d5dc5e47a65b96c90
Paul Shields <pkshields@gmail.com>
2019-05-19 14:25:58 +0100

    Removed the project item design implementation from the Projects component in favor of reusing the Project Item Component.

--- src/app/pages/projects/projects.component.spec.ts
+++ src/app/pages/projects/projects.component.spec.ts
@@ -2,6 +2,7 @@ import { ProjectsComponent } from './projects.component';
 import { async, TestBed } from '@angular/core/testing';
 
 import { RouterTestingModule } from '@angular/router/testing';
+import { ProjectItemComponent } from 'src/app/components/project-item/project-item.component';
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
@@ -13,7 +14,8 @@ describe('ProjectsComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        ProjectsComponent
+        ProjectsComponent,
+        ProjectItemComponent
       ],
     }).compileComponents();
 
