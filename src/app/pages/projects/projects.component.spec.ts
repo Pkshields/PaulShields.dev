4ca83dfcdecfd27dd79896d0e86b5ac6f0649634
Paul Shields <pkshields@gmail.com>
2019-07-06 23:35:38 +0100

    Added some tests to the root Projects component.

--- src/app/pages/projects/projects.component.spec.ts
+++ src/app/pages/projects/projects.component.spec.ts
@@ -1,6 +1,8 @@
 import { ProjectItemComponent } from 'src/app/components/project-item/project-item.component';
 
-import { TestBed, async } from '@angular/core/testing';
+import { Type } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ModalComponent } from './../../components/modal/modal.component';
@@ -27,6 +29,7 @@ import { ProjectsComponent } from './projects.component';
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
   let dom: HTMLElement;
+  let fixture: ComponentFixture<ProjectsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -60,7 +63,7 @@ describe('ProjectsComponent', () => {
       ],
     }).compileComponents();
 
-    const fixture = TestBed.createComponent(ProjectsComponent);
+    fixture = TestBed.createComponent(ProjectsComponent);
     fixture.detectChanges();
 
     component = fixture.debugElement.componentInstance;
@@ -70,4 +73,77 @@ describe('ProjectsComponent', () => {
   it('should create the projects component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should include the bombcast radio project component', () => {
+    assertThatThisPageIncludesComponent(BombcastRadioProjectComponent);
+  });
+
+  it('should include the brainfuck project component', () => {
+    assertThatThisPageIncludesComponent(BrainfuckProjectComponent);
+  });
+
+  it('should include the bulletheli project component', () => {
+    assertThatThisPageIncludesComponent(BulletHeliProjectComponent);
+  });
+
+  it('should include the foodiy project component', () => {
+    assertThatThisPageIncludesComponent(FooDIYProjectComponent);
+  });
+
+  it('should include the game frameworkd project component', () => {
+    assertThatThisPageIncludesComponent(GameFrameworksProjectComponent);
+  });
+
+  it('should include the game of life project component', () => {
+    assertThatThisPageIncludesComponent(GameOfLifeProjectComponent);
+  });
+
+  it('should include the gbps3 project component', () => {
+    assertThatThisPageIncludesComponent(GBPS3ProjectComponent);
+  });
+
+  it('should include the kineckxna project component', () => {
+    assertThatThisPageIncludesComponent(KinectXNAProjectComponent);
+  });
+
+  it('should include the muscreenmanager project component', () => {
+    assertThatThisPageIncludesComponent(MuScreenManagerProjectComponent);
+  });
+
+  it('should include the particle system project component', () => {
+    assertThatThisPageIncludesComponent(ParticleSystemProjectComponent);
+  });
+
+  it('should include the physics4k project component', () => {
+    assertThatThisPageIncludesComponent(Physics4KProjectComponent);
+  });
+
+  it('should include the super easy php templating project component', () => {
+    assertThatThisPageIncludesComponent(SuperEasyPHPTemplatingProjectComponent);
+  });
+
+  it('should include the the pixel underground project component', () => {
+    assertThatThisPageIncludesComponent(ThePixelUndergroundProjectComponent);
+  });
+
+  it('should include the undead redemption project component', () => {
+    assertThatThisPageIncludesComponent(UndeadRedemptionProjectComponent);
+  });
+
+  it('should include the vlappy man project component', () => {
+    assertThatThisPageIncludesComponent(VlappyManProjectComponent);
+  });
+
+  it('should include the web physics project component', () => {
+    assertThatThisPageIncludesComponent(WebPhysicsProjectComponent);
+  });
+
+  it('should include the windows 8 apps project component', () => {
+    assertThatThisPageIncludesComponent(Windows8AppsProjectComponent);
+  });
+
+  function assertThatThisPageIncludesComponent(T: Type<any>): any {
+    const childComponent = fixture.debugElement.query(By.directive(T)).componentInstance;
+    expect(childComponent).toBeTruthy();
+  }
 });
