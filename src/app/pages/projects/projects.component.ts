f25880e45b23208d36c1887153fd2a6647adc6b7
Paul Shields <pkshields@gmail.com>
2019-11-29 00:10:26 +0000

    Fixing alignment of the Project items on mobile.

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss', './projects.component.mobile.scss']
 })
 export class ProjectsComponent { }
